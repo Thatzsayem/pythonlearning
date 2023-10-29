ea9f2ff0b44ba18621a43f03280c225257f07927
ThatzSayem <thatzsayemchowdhury@gmail.com>
2023-10-29 11:19:21 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+number = int(input("Which number do you want to change ?"))
+test = number / 2
+c = number % 2
+print(c)
+if number % 2 == 0:
+    print("This is even number")
+else:
+    print("This is odd number")
